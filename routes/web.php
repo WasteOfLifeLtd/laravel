530f46968e94e98d4e34565479dfded56c5af9ec
serenewayfaringstranger <serenewayfaringstranger@gmail.com>
2019-02-25 11:30:04 +0300

    Add answers

--- routes/web.php
+++ routes/web.php
@@ -20,4 +20,5 @@ Auth::routes();
 Route::get('/home', 'HomeController@index')->name('home');
 
 Route::resource('questions', 'QuestionsController')->except('show');
+Route::post('/questions/{question}/answers', 'AnswersController@store')->name('answers.store');
 Route::get('/questions/{slug}', 'QuestionsController@show')->name('questions.show');
